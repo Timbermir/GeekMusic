a9228ee513c3725e4060c3ba99d28bd200872025
albertik001 <albert.nurlanovv@mail.ru>
2022-08-07 15:37:48 +0600

    Added playlistsFragment

---
+++ feature-main/src/main/java/com/timberta/geekmusic/feature_main/presentation/ui/fragments/playlists/PlaylistsViewModel.kt
@@ -0,0 +1,6 @@
+package com.timberta.geekmusic.feature_main.presentation.ui.fragments.playlists
+
+import com.timberta.geekmusic.core.base.BaseViewModel
+
+class PlaylistsViewModel : BaseViewModel() {
+}
