4310fac42d6b9d738aafc2d41e3327146a2a22b7
Clar1fy <timplifier@gmail.com>
2022-08-09 00:39:02 +0600

    moved

--- feature-main/src/main/java/com/timberta/geekmusic/feature_main/presentation/ui/fragments/playlists/PlaylistsViewModel.kt
+++
@@ -1,6 +0,0 @@
-package com.timberta.geekmusic.feature_main.presentation.ui.fragments.playlists
-
-import com.timberta.geekmusic.core.base.BaseViewModel
-
-class PlaylistsViewModel : BaseViewModel() {
-}
